098f3391b45d2abf30f3645fad947b2a74b354c2
Root007x <mahadihasanpm@gmail.com>
2025-09-13 11:57:07 +0600

    added

--- app/api/v1/endpoints/lab_report.py
+++ app/api/v1/endpoints/lab_report.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, HTTPException, Depends, UploadFile, File
+from fastapi import APIRouter, HTTPException, Depends, UploadFile, File, BackgroundTasks
 from fastapi.responses import JSONResponse
 import os
 import shutil
+import json
 
 from app.config import TEMP_FOLDER_NAME
 from app.services.lab_report.ocr.ocr import OCR
+from app.services.lab_report.graph.graph import LabReportGraph
+from app.services.lab_report.llms.onenai_llm import OpenAILLM
 
 
 router = APIRouter()
@@ -12,10 +15,16 @@ router = APIRouter()
 def get_ocr():
     return OCR()
 
+def get_graph():
+    llm_model = OpenAILLM()
+    llm = llm_model.get_llm_model()
+    graph = LabReportGraph(model=llm)
+    return graph
+
 
 
 @router.post("/lab_report_analysis")
-async def lab_report(file : UploadFile = File(...), ocr : OCR = Depends(get_ocr)):
+async def lab_report(file : UploadFile = File(...), ocr = Depends(get_ocr), graph = Depends(get_graph)):
     
     allowed_file_types = ["image/jpeg","image/png","image/bmp", "application/pdf"]
     
@@ -26,30 +35,42 @@ async def lab_report(file : UploadFile = File(...), ocr : OCR = Depends(get_ocr)
     os.makedirs(TEMP_FOLDER_NAME, exist_ok=True)
     temp_file_path = os.path.join(TEMP_FOLDER_NAME, file.filename)
     
-    with open(temp_file_path, "wb") as f:
-        shutil.copyfileobj(file.file, f)
-        
-    ### Classify File
-    if file.content_type == "application/pdf":
-        print("PDF calling..........")
-        
-        pdf_data = ocr.pdf_to_text(pdf_file=temp_file_path)
-        
-        return JSONResponse({
-            "file_name" : file.filename,
-            "pdf_to_text" : pdf_data
-        })
-    elif file.content_type.startswith("image/"):
-        print("Image calling.........")
-        
-        img_data = ocr.img_to_text(img_file=temp_file_path)
-        
-        return JSONResponse({
-            "file_name" : file.filename,
-            "img_to_text" : img_data
-        })
-    else:
-        return JSONResponse({
-            "Error" : "Something Went Wrong!!!!"
-        })
+    try:   
+        with open(temp_file_path, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+            
+        # Graph setup
+        graph_builder = graph.setup_graph()
+            
+        ### Classify File
+        if file.content_type == "application/pdf":
+            print("PDF calling..........")
+            
+            pdf_data = ocr.pdf_to_text(pdf_file=temp_file_path)
+            pdf_text = graph_builder.invoke({
+                "report_text" : pdf_data
+            })
+            
+            return JSONResponse({
+                "file_name" : file.filename,
+                "pdf_to_text" : json.loads(pdf_text["output"])
+            })
+        elif file.content_type.startswith("image/"):
+            print("Image calling.........")
+            
+            img_data = ocr.img_to_text(img_file=temp_file_path)
+            img_text = graph_builder.invoke({
+                "report_text" : img_data
+            })
+            
+            return JSONResponse({
+                "file_name" : file.filename,
+                "img_to_text" : json.loads(img_text["output"])
+            })
+        else:
+            return JSONResponse({
+                "Error" : "Something Went Wrong!!!!"
+            })
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
         
