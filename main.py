e88aafff12080d027e3bddd212220437711fcc47
Root007x <mahadihasanpm@gmail.com>
2025-09-22 12:45:11 +0600

    added

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 import uvicorn
 from app.api.v1.endpoints import lab_report
 from app.api.v1.endpoints import meal_scaner
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Router Include
 app.include_router(lab_report.router)
@@ -11,4 +21,4 @@ app.include_router(meal_scaner.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
